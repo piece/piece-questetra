f9c2e761e32fa15f96c753ed8b3886fad083ae7f
KUBO Atsuhiro <kubo@iteman.jp>
2012-04-22 00:59:06 +0900

    Updated the path of Composer's autoload.php.

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -47,7 +47,7 @@ spl_autoload_register(function ($class) {
     }
 });
 
-require_once __DIR__ . '/../vendor/.composer/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 /*
  * Local Variables:
